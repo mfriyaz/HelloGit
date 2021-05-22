abbf70aceecf83f807f7722b94f2a6c90121eec1
Riyaz <riyaz@awnsys.com>
2021-05-22 18:50:22 +0800

    Commit 01

---
+++ HelloGit.java
@@ -0,0 +1,7 @@
+public class HelloGit {
+	  public static void main(String[] args) {
+                       // TODO Auto-generated method stub
+                            System.out.println("Hello Agni") ;
+                            System.out.println("Hello Riyaz") ;
+    }
+}
